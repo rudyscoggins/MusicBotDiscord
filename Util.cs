726a38b81cb944b7ed7eb118a9ba1e03c728d425
rudyscoggins <rudyscoggins@gmail.com>
2023-05-13 15:31:06 -0500

    Initial code commit, bot lives but does nothing

---
+++ Util.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace MusicBotDiscord;
+public class Util
+{
+
+private static readonly string dataLocation = "data/";
+
+public static string GetTokenFromFile(){
+    
+    string json = System.IO.File.ReadAllText(@$"{dataLocation}credentials.json");
+
+      // Parse the JSON string into a JObject
+      JObject obj = JObject.Parse(json);
+            
+      return (string)obj["token"];
+
+    
+    //  You can assign your bot token to a string, and pass that in to connect.
+    //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+    //return "MTEwNTMyNzUzOTUyNzg4ODk0Nw.Gz60x7.bg1AUarJPMUBnx9d8gJsmCVilqOgrSTuUI4mXU";
+}
+}
